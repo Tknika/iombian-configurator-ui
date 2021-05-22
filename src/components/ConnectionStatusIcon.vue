ec6d5b700046dabc8881f5192fdcd9daf6f61356
Aitor Iturrioz <riturrioz@gmail.com>
2021-05-22 19:58:22 +0200

    Add connection status tooltip
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/components/ConnectionStatusIcon.vue
+++ src/components/ConnectionStatusIcon.vue
@@ -1,8 +1,15 @@
 <template>
-  <v-icon :color="connectionStatusColor" right>{{ connectionStatusIcon }}</v-icon>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <v-icon :color="connectionStatusColor" right v-bind="attrs" v-on="on">{{ connectionStatusIcon }}</v-icon>
+    </template>
+    <span>{{ connectionStatusMessage }}</span>
+  </v-tooltip>
 </template>
 
 <script>
+import strftime from "strftime";
+
 export default {
   name: "ConnectionStatusIcon",
   props: {
@@ -15,15 +22,18 @@ export default {
     statusMap: {  "online": { "icon": "mdi-check-circle", "color": "green" },
                   "warning": { "icon": "mdi-alert-circle", "color": "orange" },
                   "offline": { "icon": "mdi-alert-circle", "color": "red"},
-                  "unknown": { "icon": "help-circle", "color": "blue"},
+                  "unknown": { "icon": "mdi-help-circle", "color": "grey"},
                 },
     nowTimestamp: Date.now(),
     nowTimestampTimer: null,
     nowTimestampRefreshPeriodMs: 60000,
   }),
   computed: {
+    lastConnectionFormated() {
+      return strftime("%Y-%m-%d %H:%M:%S", new Date(this.lastConnection*1000));
+    },
     lastConnectionElapsedHours() {
-      return (Math.round(this.nowTimestamp / 1000) - this.lastConnection) / 3600;
+      return this.lastConnection < 0 ? -1 : (Math.round(this.nowTimestamp / 1000) - this.lastConnection) / 3600;
     },
     connectionStatus() {
       const elapsedHours = this.lastConnectionElapsedHours;
@@ -36,7 +46,21 @@ export default {
     connectionStatusColor() {
       const status = this.connectionStatus;
       return this.statusMap[status]["color"]
-    },       
+    },
+    connectionStatusMessage() {
+      switch (this.connectionStatus) {
+        case "online":
+          return "Connected device";
+        case "warning":
+          return "Last connection >1 hour ago";
+        case "offline":
+          return `Last connection: ${this.lastConnectionFormated}`;
+        case "unknown":
+          return "Never connected or unsupported device"
+        default:
+          return "No information";
+      }
+    },
   },
   created() {
     this.nowTimestampTimer = setInterval(this.refreshNowTimestamp, this.nowTimestampRefreshPeriodMs);
