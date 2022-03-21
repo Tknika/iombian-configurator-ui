c154fd2c71f1e5eeea00155cc72dd2809863f581
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-03-21 23:41:14 +0100

    Emit connectionStatusChanged event for device
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- src/components/ConnectionStatusIcon.vue
+++ src/components/ConnectionStatusIcon.vue
@@ -1,7 +1,9 @@
 <template>
   <v-tooltip bottom>
     <template v-slot:activator="{ on, attrs }">
-      <v-icon :color="connectionStatusColor" right v-bind="attrs" v-on="on">{{ connectionStatusIcon }}</v-icon>
+      <v-icon :color="connectionStatusColor" right v-bind="attrs" v-on="on">{{
+        connectionStatusIcon
+      }}</v-icon>
     </template>
     <span>{{ connectionStatusMessage }}</span>
   </v-tooltip>
@@ -14,33 +16,48 @@ export default {
   name: "ConnectionStatusIcon",
   props: {
     lastConnection: {
-        type: Number,
-        required: true,
-    }
+      type: Number,
+      required: true,
+    },
   },
   data: () => ({
-    statusMap: {  "online": { "icon": "mdi-cloud-check", "color": "green" },
-                  "warning": { "icon": "mdi-cloud-alert", "color": "orange" },
-                  "offline": { "icon": "mdi-cloud-alert", "color": "red"},
-                  "non-connected": { "icon": "mdi-cloud-question", "color": "grey"},
-                  "legacy": { "icon": "mdi-help-circle", "color": "grey"},
-                },
+    statusMap: {
+      online: { icon: "mdi-cloud-check", color: "green" },
+      warning: { icon: "mdi-cloud-alert", color: "orange" },
+      offline: { icon: "mdi-cloud-alert", color: "red" },
+      "non-connected": { icon: "mdi-cloud-question", color: "grey" },
+      legacy: { icon: "mdi-help-circle", color: "grey" },
+    },
     nowTimestamp: Date.now(),
     nowTimestampTimer: null,
     nowTimestampRefreshPeriodMs: 60000,
+    nowTimestampFirstPeriodMs: 100,
   }),
   computed: {
     lastConnectionFormated() {
-      return strftime("%Y-%m-%d %H:%M:%S", new Date(this.lastConnection*1000));
+      return strftime(
+        "%Y-%m-%d %H:%M:%S",
+        new Date(this.lastConnection * 1000)
+      );
     },
     lastConnectionElapsedHours() {
       if (this.lastConnection == -1) return -2; // Legacy device
       if (this.lastConnection == 0) return -1; // Non connected device
-      return (Math.round(this.nowTimestamp / 1000) - this.lastConnection) / 3600;
+      return (
+        (Math.round(this.nowTimestamp / 1000) - this.lastConnection) / 3600
+      );
     },
     connectionStatus() {
       const elapsedHours = this.lastConnectionElapsedHours;
-      return elapsedHours == -2 ? "legacy" : elapsedHours == -1 ? "non-connected" : elapsedHours < 1 ? "online" : elapsedHours < 2 ? "warning" : "offline"
+      return elapsedHours == -2
+        ? "legacy"
+        : elapsedHours == -1
+        ? "non-connected"
+        : elapsedHours < 1
+        ? "online"
+        : elapsedHours < 2
+        ? "warning"
+        : "offline";
     },
     connectionStatusIcon() {
       const status = this.connectionStatus;
@@ -48,7 +65,7 @@ export default {
     },
     connectionStatusColor() {
       const status = this.connectionStatus;
-      return this.statusMap[status]["color"]
+      return this.statusMap[status]["color"];
     },
     connectionStatusMessage() {
       switch (this.connectionStatus) {
@@ -59,16 +76,20 @@ export default {
         case "offline":
           return `Last connection: ${this.lastConnectionFormated}`;
         case "non-connected":
-          return "Never connected device"
+          return "Never connected device";
         case "legacy":
-          return "Legacy device"
+          return "Legacy device";
         default:
           return "No information";
       }
     },
   },
   created() {
-    this.nowTimestampTimer = setInterval(this.refreshNowTimestamp, this.nowTimestampRefreshPeriodMs);
+    setTimeout(this.refreshNowTimestamp, this.nowTimestampFirstPeriodMs);
+    this.nowTimestampTimer = setInterval(
+      this.refreshNowTimestamp,
+      this.nowTimestampRefreshPeriodMs
+    );
   },
   destroyed() {
     this.cancelNowTimestampTimer();
@@ -79,7 +100,15 @@ export default {
     },
     cancelNowTimestampTimer() {
       clearInterval(this.nowTimestampTimer);
-    }
-  }
-}
+    },
+  },
+  watch: {
+    connectionStatus: {
+      deep: true,
+      handler: function (newStatus) {
+        this.$emit("connectionStatusUpdated", newStatus);
+      },
+    },
+  },
+};
 </script>
