994fdefbb79931671b4631fe2ad486dbc273ba00
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-11-11 10:25:08 +0100

    Move reload logic to AppBar
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -3,11 +3,12 @@
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <h2>
-          IoMBian Configurator
+          {{ appName }}
         </h2>
       </div>
       <v-spacer></v-spacer>
-      <v-btn v-if="userId && !$vuetify.breakpoint.xs" text @click="logoutUser">Log Out<v-icon right>mdi-logout</v-icon></v-btn>
+      <v-btn v-if="needRefresh" text color="yellow" @click="updateServiceWorker">{{reloadText}}</v-btn>
+      <v-btn v-if="userId && !$vuetify.breakpoint.xs" text @click="logoutUser">{{ logOutText }}<v-icon right>mdi-logout</v-icon></v-btn>
       <v-btn v-if="userId && $vuetify.breakpoint.xs" icon @click="logoutUser"><v-icon>mdi-logout</v-icon></v-btn>
     </v-app-bar>
   </div>
@@ -16,7 +17,16 @@
 <script>
 import firebase from "firebase/app";
 import "firebase/auth";
+import useRegisterSW from '@/mixins/useRegisterSW'
+
 export default {
+  mixins: [useRegisterSW],
+  data: () => ({
+    appName: "IoMBian Configurator",
+    logOutText: "Log Out",
+    reloadText: "Reload",
+    reloadMessage: "New content available, click on reload button to update."
+  }),
   computed: {
     userId() {
       return this.$store.state.user.id;
