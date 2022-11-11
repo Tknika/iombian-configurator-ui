5f7965bcc5e2e2b178a7390efe4d83de81e601b2
Aitor Iturrioz <aiturrioz@tknika.eus>
2022-11-11 12:30:39 +0100

    Add PWA install button
    Signed-off-by: Aitor Iturrioz <aiturrioz@tknika.eus>

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,8 +6,11 @@
           {{ appName }}
         </h2>
       </div>
+      <v-btn v-if="pwaInstallPrompt && !$vuetify.breakpoint.xs" class="ml-2" icon @click="installPWA"><v-icon>mdi-monitor-arrow-down</v-icon></v-btn>
+      <v-btn v-if="pwaInstallPrompt && $vuetify.breakpoint.xs" class="ml-2" icon @click="installPWA"><v-icon>mdi-cellphone-arrow-down</v-icon></v-btn>
       <v-spacer></v-spacer>
-      <v-btn v-if="needRefresh" text color="yellow" @click="updateServiceWorker">{{reloadText}}</v-btn>
+      <v-btn v-if="needRefresh && !$vuetify.breakpoint.xs" text @click="updateServiceWorker">{{reloadText}}<v-icon right>mdi-reload-alert</v-icon></v-btn>
+      <v-btn v-if="needRefresh && $vuetify.breakpoint.xs" icon @click="updateServiceWorker"><v-icon>mdi-reload-alert</v-icon></v-btn>
       <v-btn v-if="userId && !$vuetify.breakpoint.xs" text @click="logoutUser">{{ logOutText }}<v-icon right>mdi-logout</v-icon></v-btn>
       <v-btn v-if="userId && $vuetify.breakpoint.xs" icon @click="logoutUser"><v-icon>mdi-logout</v-icon></v-btn>
     </v-app-bar>
@@ -25,8 +28,18 @@ export default {
     appName: "IoMBian Configurator",
     logOutText: "Log Out",
     reloadText: "Reload",
-    reloadMessage: "New content available, click on reload button to update."
+    reloadMessage: "New content available, click on reload button to update.",
+    pwaInstallPrompt: null,
   }),
+  created() {
+    window.addEventListener("beforeinstallprompt", e => {
+      e.preventDefault();
+      this.pwaInstallPrompt = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      this.pwaInstallPrompt = null;
+    });
+  },
   computed: {
     userId() {
       return this.$store.state.user.id;
@@ -42,6 +55,9 @@ export default {
       }
       this.$store.dispatch("user/clearUserData");
       this.$router.replace({ name: "SignIn" });
+    },
+    async installPWA() {
+      this.pwaInstallPrompt.prompt();
     },
   },
 };
