17725931cf73dd9f0fda0506708e0d4fed7673a7
Aitor Iturrioz <aiturrioz@tknika.eus>
2024-07-15 00:19:44 +0200

    Simplify default configuration parameters

--- src/assets/default_parameters.js
+++ src/assets/default_parameters.js
@@ -19,51 +19,8 @@ const parameters = {
             profile: "dhcp"
         },
     },
-    display: {
-        enabled: true,
-    },
     sshd: {
         enabled: true,
-    },
-    nodered: {
-        port: 1880,
-        auth: true,
-    },
-    mosquitto: {
-        port: 1883,
-        websockets: true,
-        auth: true,
-    },
-    mqttclient: {
-        port: 1888,
-    },
-    samba: {
-        enabled: true,
-    },
-    wetty: {
-        enabled: true,
-        port: 3000
-    },
-    monit: {
-        refresh: 10,
-        port: 2812,
-        alerts: {
-            fs: "90% for 5 times within 15 cycles",
-            system: {
-                load1: 6,
-                load5: 4,
-                mem: "98% for 10 cycles",
-                cpu: "95% for 10 cycles",
-                swap: "1%",
-            },
-            services: {
-                sshd: true,
-                mosquitto: true,
-                nodered: true,
-                mail: true,
-                avahi: true,
-            },
-        },
     },
     avahi: {
         ifaces: "wlan0",
